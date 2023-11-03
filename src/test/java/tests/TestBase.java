08202f2edc3028d748537ab8ad2b985638a80040
Maksim Atenyaev <atenjaev@gmail.com>
2023-11-03 09:50:41 +0600

    homework

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,7 +17,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
