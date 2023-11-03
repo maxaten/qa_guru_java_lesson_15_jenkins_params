b2f624a59a70f9e1fc247e8c2096e73fe943ef3a
Maksim Atenyaev <atenjaev@gmail.com>
2023-11-03 15:54:15 +0600

    homework

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,10 +14,12 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-//        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser =  System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.00");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
