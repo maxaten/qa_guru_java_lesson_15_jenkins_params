79fd89873f399fa29f86a7a4fcbe441c331cdb01
Maksim Atenyaev <atenjaev@gmail.com>
2023-11-03 11:21:02 +0600

    homework

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,7 +14,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
+//        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
